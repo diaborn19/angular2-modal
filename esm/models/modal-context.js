d88d64fe07957414dfd80c26dbb66ed98acaf4b8
David Loiret <david@videolabs.io>
2017-01-05 14:25:53 +0100

    fix: AoT compilation

---
+++ esm/models/modal-context.js
@@ -0,0 +1,33 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+import { extend, arrayUnion } from './../framework/utils';
+import { OverlayContext, OverlayContextBuilder } from './overlay-context';
+export var DEFAULT_VALUES = {};
+var DEFAULT_SETTERS = [
+    'message'
+];
+export var ModalContext = (function (_super) {
+    __extends(ModalContext, _super);
+    function ModalContext() {
+        _super.apply(this, arguments);
+    }
+    return ModalContext;
+}(OverlayContext));
+/**
+ * A core context builder for a modal window instance, used to define the context upon
+ * a modal choose it's behaviour.
+ */
+export var ModalContextBuilder = (function (_super) {
+    __extends(ModalContextBuilder, _super);
+    function ModalContextBuilder(defaultValues, initialSetters, baseType) {
+        if (defaultValues === void 0) { defaultValues = undefined; }
+        if (initialSetters === void 0) { initialSetters = undefined; }
+        if (baseType === void 0) { baseType = undefined; }
+        _super.call(this, extend(DEFAULT_VALUES, defaultValues || {}), arrayUnion(DEFAULT_SETTERS, initialSetters || []), baseType);
+    }
+    return ModalContextBuilder;
+}(OverlayContextBuilder));
+//# sourceMappingURL=modal-context.js.map
